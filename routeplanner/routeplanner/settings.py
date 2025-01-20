4127a0be3ab2faf659e2d7eee3c75aab0236c04c
tamara <tstoof@sogyo.nl>
2025-01-20 14:14:37 +0100

    added more security things in settings.py

--- routeplanner/routeplanner/settings.py
+++ routeplanner/routeplanner/settings.py
@@ -28,7 +28,7 @@ ENCRYPTION_KEY = config('ENCRYPTION_KEY')
 # MONGO_DB_NAME = "hikingRoutes"
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['planyourhike.onrender.com', 'www.planyourhike.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['planyourhike.onrender.com', 'www.planyourhike.onrender.com']
 
 
 # Application definition
@@ -172,3 +172,12 @@ AUTHENTICATION_BACKENDS = [
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year in seconds
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
